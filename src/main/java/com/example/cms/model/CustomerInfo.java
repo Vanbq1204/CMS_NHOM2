0b14a838a23ade0369f945d469a4b9ba0d76efc6
Bao-gio-co-tien <129248299+Bao-gio-co-tien@users.noreply.github.com>
2025-04-09 15:09:29 +0700

    commit

--- src/main/java/com/example/cms/model/CustomerInfo.java
+++ src/main/java/com/example/cms/model/CustomerInfo.java
@@ -1,8 +1,12 @@
 package com.example.cms.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Setter
+@Getter
 @Document(collection = "customerInfo")
 public class CustomerInfo {
     @Id
@@ -12,4 +16,14 @@ public class CustomerInfo {
     private String customerPhone;
     private String customerAddress;
     private String customerStatus;
+
+    private CustomerInfo(String customerId, String customerName, String customerEmail, String customerPhone, String customerAddress, String customerStatus) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.customerPhone = customerPhone;
+        this.customerAddress = customerAddress;
+        this.customerStatus = customerStatus;
+    }
+
 }
